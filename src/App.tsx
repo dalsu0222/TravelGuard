e215b8c2616e95dcce88571d1d92d1c91f17c51f
dalsu0222 <dalsu0222@sju.ac.kr>
2024-10-14 12:07:45 +0900

    chore: Update SEO optimizations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
-import { Helmet } from "react-helmet";
+import { HelmetProvider } from "react-helmet-async";
 import router from "./router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -14,20 +14,11 @@ const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Helmet>
-        <html lang="ko" />
-        <title>TravelGuard</title>
-        <meta
-          name="description"
-          content="국가별 현지 연락처, 입국 허가요건, 사건 사고정보 등 다양한 정보를 제공합니다."
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#000000" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-      </Helmet>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </HelmetProvider>
   );
 };
 
